1facaec13750ffc8d4fd40bc8f433592dc2ec254
sebas3261 <sebassanchez3261@gmail.com>
2025-03-28 02:06:28 -0500

    won

--- src/pages/Won.jsx
+++ src/pages/Won.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import './Won.css';
 
 export default function Won() {
   return (
-    <div>
-        Ganaste
+    <div className="wonContainer">
+      <div className="confettiContainer">
+        {[...Array(20)].map((_, i) => (
+          <div key={i} className={`confetti confetti-${i % 5}`}></div>
+        ))}
+      </div>
+      
+      <div className="wonCard">
+        <div className="wonBadge">
+          <div className="wonBadgeInner">
+            <span className="wonStar">★</span>
+          </div>
+        </div>
+        
+        <h1 className="wonTitle">¡Felicidades!</h1>
+        <h2 className="wonSubtitle">¡Has ganado el Bingo Biológico!</h2>
+        
+        <div className="wonMessage">
+          <p>Has demostrado un excelente conocimiento de los términos biológicos.</p>
+          <p>¡Sigue aprendiendo y divirtiéndote!</p>
+        </div>
+        
+        <button className="wonButton" onClick={() => window.location.href = '/game'}>
+          Jugar de nuevo
+        </button>
+      </div>
+      
+      <div className="decorationLeaves">
+        <div className="leaf leaf1"></div>
+        <div className="leaf leaf2"></div>
+        <div className="leaf leaf3"></div>
+        <div className="leaf leaf4"></div>
+      </div>
     </div>
-  )
+  );
 }
